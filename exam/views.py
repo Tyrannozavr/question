fd5fdff3d73dab6f09c5ad9909d117c1174be219
Dmiv <dmitriy40647274@gmail.com>
2022-01-21 23:12:45 +0300

    adding percent of questions and time before examination

--- exam/views.py
+++ exam/views.py
@@ -1,12 +1,29 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Exam
+import datetime
 
 def index(request):
     questions = Exam.objects.filter(label=True)
     learned = Exam.objects.filter(label=False)
-    return render(request, 'exam/exam_main.html', {'questions': questions, 'learned': learned})
+    percent = len(learned)/(len(questions)+len(learned))
+    percent2 = (len(learned)-1)/(len(questions)+len(learned))
+    percent2 = round(percent2*percent*100, 2)
+    percent = round(percent*100, 2)
+    remains = time()
+    quest = remains/len(questions)
+    quest = round(quest, 2)
+    return render(request, 'exam/exam_main.html', {'questions': questions, 'learned': learned, 'percent': percent,
+                                                   'percent2': percent2, 'remains': remains, 'quest': quest})
 
+
+
+def time():
+    time1 = datetime.datetime.now()
+    time2 = datetime.datetime(2022, 2, 25)
+    delta = time2 - time1
+    remains = delta.total_seconds() // 60
+    return remains
 # Create your views here.
 def edit(request, number):
     a = Exam.objects.get(question=number)
